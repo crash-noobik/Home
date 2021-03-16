b84604ea86d4f10d83ca8008eee63e2cf5751c39
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-16 14:06:43 +0300

    Home

--- independent_work/New.java
+++ independent_work/New.java
@@ -2,7 +2,7 @@ package independent_work;
 
 import java.util.Scanner;
 
-public class Two_or_three {
+public class New {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
